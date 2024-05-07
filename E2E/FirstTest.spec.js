f2508a3e167f34e2b9a7a32848bc083e9090ebac
Murillo Lopes <murillo.lopes.oliveiraa@gmail.com>
2024-05-07 18:49:30 -0300

    Added more Test Suites

--- E2E/FirstTest.spec.js
+++ E2E/FirstTest.spec.js
@@ -1,8 +1,13 @@
 //const { test, expect } = require('@playwright/test')  //mesma coisa que o using no C#
 import { test, expect } from '@playwright/test'
 
-
+// o page é para poder usar os métodos, como se eu estivesse inicializando um objeto
 test('My first Test', async ({ page }) => {
     await page.goto('https://google.com')
     await expect(page).toHaveTitle("Google")
+});
+
+test('Second Test', async ({ page }) => {
+    await page.goto('https://globoesporte.globo.com/')
+    await expect(page).toHaveTitle("ge.globo - É esporte sempre")
 });
