0ace6a2656008ee014caa09b628b6197468a953e
Murillo Lopes <murillo.lopes.oliveiraa@gmail.com>
2024-05-27 19:04:41 -0300

    Fix GHA Build

--- E2E/HomePageTests.spec.js
+++ E2E/HomePageTests.spec.js
@@ -1,23 +1,18 @@
-//const { test, expect } = require('@playwright/test')  //mesma coisa que o using no C#
 import { test, expect } from '@playwright/test'
 
-// o page é para poder usar os métodos, como se eu estivesse inicializando um objeto
-test('My first Test', async ({ page }) => {
+test('Google Test', async ({ page }) => {
     let googleSearchBar = '#APjFqb';
-    let text;
-    let expectedTitle;
+    let localRun = false;
 
     await page.goto('https://google.com');
     await page.locator(googleSearchBar).click();
     await page.locator(googleSearchBar).fill('Globo');
     await page.keyboard.press('Enter');
     await page.pause();
-    text = 'Globo - Pesquisa Google';
 
-    if (text.includes('Globo - Pesquisa Google')) {
-        expectedTitle = "Globo - Pesquisa Google";
+    if (localRun) {
+        await expect(page).toHaveTitle('Globo - Pesquisa Google');
     } else {
-        expectedTitle = "Globo - Google Search"
+        await expect(page).toHaveTitle('Globo - Google Search');
     }
-    await expect(page).toHaveTitle(expectedTitle);
 });
