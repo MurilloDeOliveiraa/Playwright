c81e1048e78d96277cea76856d314dbbd9625c13
Murillo Lopes <murillo.lopes.oliveiraa@gmail.com>
2024-07-15 22:13:25 -0300

    new config

--- playwright.config.js
+++ playwright.config.js
@@ -27,12 +27,12 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  //retries: process.env.CI ? 2 : 0,
-  retries: 1,
+  retries: process.env.CI ? 1 : 0,
+  // retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['junit', { outputFile: 'test-results/e2e-junit-results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
