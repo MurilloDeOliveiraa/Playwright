076e0b6b6e4cbdc25ccfb45ddf0176dbdbfb1d8a
Murillo Lopes <murillo.lopes.oliveiraa@gmail.com>
2024-10-01 10:33:12 -0300

    maximized browser

--- playwright.config.js
+++ playwright.config.js
@@ -40,13 +40,19 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    launchOptions: {
+      args: ["--start-maximized"]
+    }
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        // ...devices['Desktop Chrome'] 
+        viewport: null
+      },
     },
     // {
     //   name: 'firefox',
