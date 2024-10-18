3259ac71a17525861f871f6ec812b1616270f7fb
Murillo Lopes <murillo.lopes.oliveiraa@gmail.com>
2024-10-17 22:54:01 -0300

    added Js Custom Action

---
+++ .github/actions/js-custom-action/test.js
@@ -0,0 +1,16 @@
+import core from '@actions/core';
+import github from '@actions/github';
+import exec from '@actions/exec';
+
+let states = ['SP', 'RJ', 'PR', 'RS']
+
+function showCities() {
+    for (const i of states) {
+        console.log(i)
+        core.notice(i)
+    }
+    core.notice('Hello Everyone!')
+}
+
+showCities()
+
